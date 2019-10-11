b1e32ec24e16872cbe301f94e179dfc02e228325
RAGANITHI <36047046+RAGANITHI@users.noreply.github.com>
2019-10-11 17:01:28 +0530

    TTS/STT integ-initial

--- TTS/app/src/main/java/com/example/android/tts/MainActivity.java
+++ TTS/app/src/main/java/com/example/android/tts/MainActivity.java
@@ -8,19 +8,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import java.util.ArrayList;
 import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech textToSpeech;
     private Button btn;
     private EditText editText;
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private TextView mVoiceInputTv;
+    private ImageButton mSpeakBtn;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
+
+        mVoiceInputTv = (TextView) findViewById(R.id.editText);
         editText = (EditText) findViewById(R.id.editText);
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -38,9 +54,47 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
                 }
+                String data="Hi Raga. I am your AI-Assistant,What are you looking for?";
+                int speechStatus=textToSpeech.speak(data,TextToSpeech.QUEUE_FLUSH,null);
+                if (speechStatus == TextToSpeech.ERROR){
+                    Log.e("error","errror puutukuchu");
+                }
+                mSpeakBtn.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startVoiceInput();
+                    }
+                });
+
             }
         });
 
+    }
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    editText.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     @Override
